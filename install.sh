7e960b0971b20ca51cbae5f239614588627205b0
raymond <ryang@blackbot.io>
2020-11-22 13:33:19 -0800

    enhanced install.sh

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+check_internet() {
+	wget -q --tries=10 --timeout=10 --spider https://github.com
+	if [[ $? -eq 0 ]]; then
+        	echo "Connection Found"
+	else
+        	printf "NO Connection Found\nExiting...\n"
+		exit 0
+	fi	
+}
+
+debian_install() {
+	sudo apt-get install git python3.7 python3-pip wget unzip -y
+	git clone git@gitlab.blackbot.net:ossig/artic2.git
+	cd artic2
+	pip3 install -r requirements.txt
+	sudo wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip -O /usr/local/sbin/ngrok
+	sudo unzip /usr/local/sbin/ngrok
+	sudo chmod a+x /usr/local/sbin/ngrok
+
+}
+
+arch_install() {
+	echo "y" | sudo pacman -S git python-pip python3 wget unzip
+	check_internet
+	git clone git@gitlab.blackbot.net:ossig/artic2.git
+	cd artic2
+	pip3 install -r requirements.txt
+	sudo wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip -O /usr/local/sbin/ngrok
+	sudo unzip /usr/local/sbin/ngrok
+	sudo chmod a+x /usr/local/sbin/ngrok
+}
+
+check_internet
+if [[ $(which apt-get) == "/usr/bin/apt-get" ]]
+then
+	debian_install
+elif [[ $(which pacman) == "/usr/bin/pacman" ]]
+then
+	arch_install
+fi
+
